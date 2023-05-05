e1f9f03299d2149b8592ef5ff273182a863f5f4c
robetbangunlegato <robetlegato22@mhs.mdp.ac.id>
2023-05-05 15:11:52 +0700

    buat tampilan xml

--- app/src/main/java/com/example/kampuskita/Activity/MainActivity.java
+++ app/src/main/java/com/example/kampuskita/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.kampuskita;
+package com.example.kampuskita.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.kampuskita.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/kampuskita/Activity/UbahActivity.java
@@ -0,0 +1,16 @@
+package com.example.kampuskita.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kampuskita.R;
+
+public class UbahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah);
+    }
+}
